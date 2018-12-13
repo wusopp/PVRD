5f1bf4d160b77aa53a1ed78f0e1e36253e588161
Tamarous <532487608@qq.com>
2018-12-13 20:07:45 +0800

    对代码进行重构

--- main.cpp
+++ main.cpp
@@ -5,38 +5,6 @@
 #include "../Utils/FFmpegDemuxer.h"
 #include "../NVDecoder/NvDecoder.h"
 
-void readDataFromFile(const char *fileName, unsigned char *yuvData, int frameWidth, int frameHeight) {
-	if (yuvData == NULL) {
-		return;
-	}
-	size_t size = frameWidth * frameHeight * 3 / 2;
-	FILE *pf = fopen(fileName, "rb+");
-	fread(yuvData, 1, size, pf);
-	fclose(pf);
-	return;
-}
-
-void convertYUV2RGB(unsigned char *yuvData, unsigned char *rgbData, int frameWidth, int frameHeight) {
-	if (frameWidth < 1 || frameHeight < 1 || yuvData == NULL || rgbData == NULL) {
-		return;
-	}
-	yuv420p_to_rgb24(yuvData, rgbData, frameWidth, frameHeight);
-}
-
-std::string videoFileName;
-std::string outputImageName;
-ProjectionMode projectionMode = PM_ERP;
-DrawMode drawMode = DM_USE_INDEX;
-VideoFileType videoFileType = VFT_Encoded;
-DecodeType decodeType = DT_SOFTWARE;
-int patchNum;
-int frameWidth;
-int frameHeight;
-bool renderYUV = false;
-bool repeatRendering = false;
-
-
-
 int main(int argc, char** argv) {
     
 
